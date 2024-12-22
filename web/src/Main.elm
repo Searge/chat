b437024e0a649a0171107be99fee23f81c63e6d2
Searge <searge@duck.com>
2024-12-22 21:47:06 +0200

    refactor: Restructure Elm app structure and simplify message handling
    Restructures the Elm application by:
    - Removing the `Message` type and replacing it with a simple list of strings
    - Simplifying the `update` function to handle message submission more directly
    - Removing the unused `subscriptions` function
    - Updating the `view` to use a more straightforward layout and form handling
    
    These changes make the application simpler and more focused on the core message handling functionality.

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -3,7 +3,7 @@ module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Html.Events exposing (onInput, onSubmit)
 
 
 -- MAIN
@@ -13,27 +13,23 @@ main =
     Browser.element
         { init = init
         , update = update
-        , subscriptions = subscriptions
         , view = view
+        , subscriptions = \_ -> Sub.none
         }
 
 
 -- MODEL
 
-type alias Message =
-    { id : Int
-    , text : String
+type alias Model =
+    { draft : String
+    , messages : List String
     }
 
-type alias Model =
-    { messages : List Message
-    , draft : String
-    }
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { messages = []
-      , draft = ""
+    ( { draft = ""
+      , messages = []
       }
     , Cmd.none
     )
@@ -43,7 +39,8 @@ init _ =
 
 type Msg
     = DraftChanged String
-    | SendMessage
+    | MessageSubmitted
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -53,45 +50,30 @@ update msg model =
             , Cmd.none
             )
 
-        SendMessage ->
-            if String.isEmpty (String.trim model.draft) then
-                ( model, Cmd.none )
-            else
-                ( { model
-                    | messages = model.messages ++ [ Message (List.length model.messages) model.draft ]
-                    , draft = ""
-                  }
-                , Cmd.none
-                )
-
-
--- SUBSCRIPTIONS
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
+        MessageSubmitted ->
+            ( { model
+                | messages = model.draft :: model.messages
+                , draft = ""
+              }
+            , Cmd.none
+            )
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-    div [ class "chat-container" ]
-        [ div [ class "messages" ]
-            (List.map viewMessage model.messages)
-        , div [ class "input-area" ]
+    div []
+        [ Html.form [ onSubmit MessageSubmitted ]
             [ input
                 [ type_ "text"
+                , placeholder "Type a message"
                 , value model.draft
                 , onInput DraftChanged
-                , placeholder "Type a message..."
                 ]
                 []
-            , button [ onClick SendMessage ] [ text "Send" ]
+            , button [ type_ "submit" ] [ text "Send" ]
             ]
+        , div []
+            (List.map (\msg -> p [] [ text msg ]) (List.reverse model.messages))
         ]
-
-viewMessage : Message -> Html Msg
-viewMessage message =
-    div [ class "message" ]
-        [ text message.text ]
